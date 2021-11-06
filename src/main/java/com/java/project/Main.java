3b1e9333fe1cfc1126d9886682d73070c68e6aaf
martadrabik-dabrowska <m.drabik.dabrowska@gmail.com>
2021-11-06 22:27:25 +0100

    adding main.fxml

---
+++ src/main/java/com/java/project/Main.java
@@ -0,0 +1,11 @@
+package com.java.project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
